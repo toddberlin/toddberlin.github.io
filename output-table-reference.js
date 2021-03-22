83cff0e734fdf64d964d45cd355d5edcbc3ba1b3
Todd Berlin <github@toddberl.in>
2021-03-22 14:38:59 +1000

    Create output-table-reference.js

---
+++ output-table-reference.js
@@ -0,0 +1,71 @@
+var mySkillsModes = [ // online, offsite, block
+    ["Online"],
+    ["Mobile van", "Workplace"],
+    ["Block training"]
+];
+
+var mySkillsLocations = [ //this is from MySkills
+    "Acacia Ridge",
+    "Aitkenvale",
+    "Alexandra Hills",
+    "Ashmore",
+    "Atherton",
+    "Bowen",
+    "Bracken Ridge",
+    "Browns Plains",
+    "Bundaberg",
+    "Bundamba",
+    "Burdekin",
+    "Caboolture ",
+    "Cairns",
+    "Cairns - Great Barrier Reef International Marine College",
+    "Charters Towers",
+    "Chinchilla",
+    "Cloncurry",
+    "Coolangatta",
+    "Coomera",
+    "Dalby",
+    "Eagle Farm",
+    "Grovely",
+    "Gympie",
+    "Hervey Bay",
+    "Inala",
+    "Ingham",
+    "Innisfail",
+    "Kingaroy",
+    "Loganlea",
+    "Maroochydore",
+    "Maryborough",
+    "Mooloolaba",
+    "Mount Gravatt ",
+    "Mount Isa",
+    "Nambour",
+    "Normanton",
+    "Nurunderi",
+    "Palm Island",
+    "Redcliffe",
+    "Roma",
+    "Southbank",
+    "Southport",
+    "Springfield",
+    "The Whitsundays",
+    "Thursday Island",
+    "Toowoomba",
+    "Townsville",
+    "Trade Training Centre",
+    "Warwick"
+];
+
+/*
+"Course Code"
+"Tuition Fee" // = 0
+"Duration Number" // = ""
+"Duration Type" // = ""
+"Available Part-Time" // = Y | N
+"Available Online" // = Y | N
+"Offsite Delivery Available" // = Y | N
+"Block Training Available" // = Y | N
+"Course and Additional Fees Information"// = "
+
+"RTO Course URL"
+*/
