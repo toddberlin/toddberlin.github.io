ffc1a7a73b1cf4bcb3cb6c11c90a504569cfb2ff
Todd Berlin <github@toddberl.in>
2023-03-01 15:52:33 +1000

    added new files for handling csv files directly

---
+++ employer-apprentice-reformat/script2.js
@@ -0,0 +1,143 @@
+function fileInfo(e) {
+  var file = e.target.files[0];
+  if (file.name.split(".")[1].toUpperCase() != "CSV") {
+    alert('Invalid csv file !');
+    e.target.parentNode.reset();
+    return;
+  } else {
+    document.getElementById('file_info').innerHTML = "<p>File Name: " + file.name + " | " + file.size +
+      " Bytes.</p>";
+  }
+}
+
+function handleFileSelect() {
+  var file = document.getElementById("the_file").files[0];
+  var reader = new FileReader();
+  var link_reg = /(http:\/\/|https:\/\/)/i;
+  reader.onload = function (file) {
+    var content = file.target.result;
+    var rows = file.target.result.split(/[\r\n|\n]+/);
+    var table = document.createElement('table');
+
+    for (var i = 0; i < rows.length; i++) {
+      var tr = document.createElement('tr');
+      var arr = rows[i].split(',');
+
+      for (var j = 0; j < arr.length; j++) {
+        if (i == 0)
+          var td = document.createElement('th');
+        else
+          var td = document.createElement('td');
+
+        if (link_reg.test(arr[j])) {
+          var a = document.createElement('a');
+          a.href = arr[j];
+          a.target = "_blank";
+          a.innerHTML = arr[j];
+          td.appendChild(a);
+        } else {
+          td.innerHTML = arr[j];
+        }
+        tr.appendChild(td);
+      }
+
+      table.appendChild(tr);
+    }
+
+    document.getElementById('list').appendChild(table);
+  };
+  reader.readAsText(file);
+}
+document.getElementById('the_form').addEventListener('submit', handleFileSelect, false);
+document.getElementById('the_file').addEventListener('change', fileInfo, false);
+
+
+
+
+/************************************************************************************************************************************************************************************* */
+
+
+
+
+
+var inputElement = document.getElementById('fileItem');
+var outputDiv = document.getElementById("velocity-output");
+
+function outputToDom(refactoredData, referenceList) {
+  var unitsRefOutput = JSON.stringify(referenceList, null, 2);
+  var objectOutput= JSON.stringify(refactoredData, null, 2);
+  outputDiv.innerText = "#set( $allUnits = " + unitsRefOutput + " ) \n"+ "#set( $employerData = " + objectOutput + " )";
+
+  function selectElementText(el, win) {
+    win = win || window;
+    var doc = win.document, sel, range;
+    if (win.getSelection && doc.createRange) {
+        sel = win.getSelection();
+        range = doc.createRange();
+        range.selectNodeContents(el);
+        sel.removeAllRanges();
+        sel.addRange(range);
+    } else if (doc.body.createTextRange) {
+        range = doc.body.createTextRange();
+        range.moveToElementText(el);
+        range.select();
+    }
+  }
+  selectElementText(outputDiv);
+  outputDiv.scrollIntoView();
+
+}
+
+function generateOutput(data) {
+  var unitsSummary = [];
+  var velOutput = [];
+
+  for (var i = 0; i < data.length; i++) { // looping through all uploaded data
+    var empEmail = data[i]["Employer Email"];
+    var apprentice = data[i]["Student Name"];
+    var unitCode = data[i]["Unit Code"];
+    var unitTitle = data[i]["Unit Study Package Full Title"];
+
+    if (velOutput.find(o => o["Email"] === empEmail)) { // check for existing emplployers 
+      var existingEmployer = velOutput.find(o => o["Email"] === empEmail); 
+      
+      if (existingEmployer.Apprentices.find(o => o["name"] === apprentice)) { // check for apprentice and add unit only
+        var existingApprentice = existingEmployer.Apprentices.find(o => o["name"] === apprentice);
+        existingApprentice.units.push(unitCode);
+      } else { // if new apprentice add new
+        existingEmployer.Apprentices.push({"name":apprentice, "units": [unitCode]});
+      }
+
+    } else { // add new employer if not exists
+      velOutput.push({
+        "Email": empEmail,
+        "Apprentices": [{
+          "name": apprentice,
+          "units": [unitCode]
+        }]
+      });
+    }
+
+    if (!unitsSummary.find(o => o["code"] === unitCode)) { // add unit to unitsSummary if not exists
+      unitsSummary.push({
+        "code": unitCode,
+        "title": unitTitle
+      });
+    }
+
+  }
+  outputToDom(velOutput, unitsSummary);
+}
+
+
+function onChange(event) {
+  var reader = new FileReader();
+  reader.onload = onReaderLoad;
+  reader.readAsText(event.target.files[0]);
+}
+function onReaderLoad(event) {
+  var obj = JSON.parse(event.target.result);
+  generateOutput(obj);
+}
+
+inputElement.addEventListener('change', onChange);
