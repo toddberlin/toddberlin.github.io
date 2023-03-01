05bf2fda66bbd83e1baf2ac9755fb34a7cc150ec
Todd Berlin <github@toddberl.in>
2023-03-01 17:06:12 +1000

    changed functionality to accept csv and now uses Papa Parse library

--- employer-apprentice-reformat/script2.js
+++ employer-apprentice-reformat/script2.js
@@ -10,57 +10,7 @@ function fileInfo(e) {
   }
 }
 
-function handleFileSelect() {
-  var file = document.getElementById("the_file").files[0];
-  var reader = new FileReader();
-  var link_reg = /(http:\/\/|https:\/\/)/i;
-  reader.onload = function (file) {
-    var content = file.target.result;
-    var rows = file.target.result.split(/[\r\n|\n]+/);
-    var table = document.createElement('table');
-
-    for (var i = 0; i < rows.length; i++) {
-      var tr = document.createElement('tr');
-      var arr = rows[i].split(',');
-
-      for (var j = 0; j < arr.length; j++) {
-        if (i == 0)
-          var td = document.createElement('th');
-        else
-          var td = document.createElement('td');
-
-        if (link_reg.test(arr[j])) {
-          var a = document.createElement('a');
-          a.href = arr[j];
-          a.target = "_blank";
-          a.innerHTML = arr[j];
-          td.appendChild(a);
-        } else {
-          td.innerHTML = arr[j];
-        }
-        tr.appendChild(td);
-      }
-
-      table.appendChild(tr);
-    }
-
-    document.getElementById('list').appendChild(table);
-  };
-  reader.readAsText(file);
-}
-document.getElementById('the_form').addEventListener('submit', handleFileSelect, false);
-document.getElementById('the_file').addEventListener('change', fileInfo, false);
-
-
-
-
-/************************************************************************************************************************************************************************************* */
-
-
-
-
-
-var inputElement = document.getElementById('fileItem');
+//var inputElement = document.getElementById('the_file');
 var outputDiv = document.getElementById("velocity-output");
 
 function outputToDom(refactoredData, referenceList) {
@@ -129,7 +79,7 @@ function generateOutput(data) {
   outputToDom(velOutput, unitsSummary);
 }
 
-
+/*
 function onChange(event) {
   var reader = new FileReader();
   reader.onload = onReaderLoad;
@@ -138,6 +88,17 @@ function onChange(event) {
 function onReaderLoad(event) {
   var obj = JSON.parse(event.target.result);
   generateOutput(obj);
-}
+}*/
 
-inputElement.addEventListener('change', onChange);
+document.getElementById('the_file').addEventListener('change', fileInfo, false);
+
+document.getElementById('the_form_submit').addEventListener('click', () => {
+  Papa.parse(document.getElementById('the_file').files[0],{
+    download: true,
+    header: true,
+    skipEmptyLines: true,
+    complete: function(results){
+      generateOutput(results.data);
+    }
+  });
+});
