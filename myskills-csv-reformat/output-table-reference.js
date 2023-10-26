b04fe8a9b859f6d0bf44fff572e7cc00667007c7
Todd Berlin <github@toddberl.in>
2023-10-26 13:47:16 +1000

    swapped to papaParse

--- myskills-csv-reformat/output-table-reference.js
+++ myskills-csv-reformat/output-table-reference.js
@@ -4,7 +4,7 @@ var mySkillsModes = [ // online, offsite, block
     ["Block training"]
 ];
 
-var mySkillsWorkloads = [ "Full time" ]; //
+var mySkillsWorkloads = [ "full time" ]; //
 
 var mySkillsLocations = [ //this order is from MySkills - text match exactly to TQOne values
     "Acacia Ridge",
@@ -57,6 +57,203 @@ var mySkillsLocations = [ //this order is from MySkills - text match exactly to 
     "Townsville (Trade Training Centre Bohle)",
     "Warwick"
 ];
+
+var mySkillsLocations2 = [{
+    "mySkillsLabel": "Offer at: Acacia Ridge (Location ID: 38087)",
+    "tqOneLabel": "Acacia Ridge"
+},
+{
+    "mySkillsLabel": "Offer at: Aitkenvale (Location ID: 36633)",
+    "tqOneLabel": "Aitkenvale"
+},
+{
+    "mySkillsLabel": "Offer at: Alexandra Hills (Location ID: 35213)",
+    "tqOneLabel": "Alexandra Hills"
+},
+{
+    "mySkillsLabel": "Offer at: Ashmore (Location ID: 36666)",
+    "tqOneLabel": "Ashmore"
+},
+{
+    "mySkillsLabel": "Offer at: Atherton (Location ID: 36632)",
+    "tqOneLabel": "Atherton"
+},
+{
+    "mySkillsLabel": "Offer at: Bowen (Location ID: 36634)",
+    "tqOneLabel": "Bowen"
+},
+{
+    "mySkillsLabel": "Offer at: Bracken Ridge (Location ID: 35702)",
+    "tqOneLabel": "Bracken Ridge"
+},
+{
+    "mySkillsLabel": "Offer at: Browns Plains (Location ID: 36658)",
+    "tqOneLabel": "Browns Plains"
+},
+{
+    "mySkillsLabel": "Offer at: Bundaberg (Location ID: 36659)",
+    "tqOneLabel": "Bundaberg"
+},
+{
+    "mySkillsLabel": "Offer at: Bundamba (Location ID: 36648)",
+    "tqOneLabel": "Ipswich"
+},
+{
+    "mySkillsLabel": "Offer at: Burdekin (Location ID: 36635)",
+    "tqOneLabel": "Burdekin"
+},
+{
+    "mySkillsLabel": "Offer at: Caboolture  (Location ID: 35703)",
+    "tqOneLabel": "Caboolture"
+},
+{
+    "mySkillsLabel": "Offer at: Cairns (Location ID: 36636)",
+    "tqOneLabel": "Cairns"
+},
+{
+    "mySkillsLabel": "Offer at: Cairns - Great Barrier Reef International Marine College (Location ID: 36637)",
+    "tqOneLabel": "Great Barrier Reef International Marine College"
+},
+{
+    "mySkillsLabel": "Offer at: Charters Towers (Location ID: 36638)",
+    "tqOneLabel": "Charters Towers"
+},
+{
+    "mySkillsLabel": "Offer at: Chinchilla (Location ID: 36649)",
+    "tqOneLabel": "Chinchilla"
+},
+{
+    "mySkillsLabel": "Offer at: Cloncurry (Location ID: 36639)",
+    "tqOneLabel": "Cloncurry"
+},
+{
+    "mySkillsLabel": "Offer at: Coolangatta (Location ID: 36667)",
+    "tqOneLabel": "Coolangatta"
+},
+{
+    "mySkillsLabel": "Offer at: Coomera (Location ID: 36668)",
+    "tqOneLabel": "Coomera"
+},
+{
+    "mySkillsLabel": "Offer at: Dalby (Location ID: 36650)",
+    "tqOneLabel": "Dalby"
+},
+{
+    "mySkillsLabel": "Offer at: Eagle Farm (Location ID: 38088)",
+    "tqOneLabel": "Eagle Farm"
+},
+{
+    "mySkillsLabel": "Offer at: Grovely (Location ID: 35704)",
+    "tqOneLabel": "Grovely"
+},
+{
+    "mySkillsLabel": "Offer at: Gympie (Location ID: 36660)",
+    "tqOneLabel": "Gympie"
+},
+{
+    "mySkillsLabel": "Offer at: Hervey Bay (Location ID: 36661)",
+    "tqOneLabel": "Hervey Bay"
+},
+{
+    "mySkillsLabel": "Offer at: Inala (Location ID: 36651)",
+    "tqOneLabel": "Inala"
+},
+{
+    "mySkillsLabel": "Offer at: Ingham (Location ID: 36640)",
+    "tqOneLabel": "Ingham"
+},
+{
+    "mySkillsLabel": "Offer at: Innisfail (Location ID: 36641)",
+    "tqOneLabel": "Innisfail"
+},
+{
+    "mySkillsLabel": "Offer at: Kingaroy (Location ID: 36652)",
+    "tqOneLabel": "Kingaroy"
+},
+{
+    "mySkillsLabel": "Offer at: Loganlea (Location ID: 35705)",
+    "tqOneLabel": "Loganlea"
+},
+{
+    "mySkillsLabel": "Offer at: Maroochydore (Location ID: 36662)",
+    "tqOneLabel": "Maroochydore"
+},
+{
+    "mySkillsLabel": "Offer at: Maryborough (Location ID: 36663)",
+    "tqOneLabel": "Maryborough"
+},
+{
+    "mySkillsLabel": "Offer at: Mooloolaba (Location ID: 36664)",
+    "tqOneLabel": "Mooloolaba"
+},
+{
+    "mySkillsLabel": "Offer at: Mount Gravatt  (Location ID: 35706)",
+    "tqOneLabel": "Mt Gravatt"
+},
+{
+    "mySkillsLabel": "Offer at: Mount Isa (Location ID: 36642)",
+    "tqOneLabel": "Mount Isa"
+},
+{
+    "mySkillsLabel": "Offer at: Nambour (Location ID: 36665)",
+    "tqOneLabel": "Nambour"
+},
+{
+    "mySkillsLabel": "Offer at: Normanton (Location ID: 36643)",
+    "tqOneLabel": "Normanton"
+},
+{
+    "mySkillsLabel": "Offer at: Nurunderi (Location ID: 36653)",
+    "tqOneLabel": "Nurunderi"
+},
+{
+    "mySkillsLabel": "Offer at: Palm Island (Location ID: 36644)",
+    "tqOneLabel": "Palm Island"
+},
+{
+    "mySkillsLabel": "Offer at: Redcliffe (Location ID: 35707)",
+    "tqOneLabel": "Redcliffe"
+},
+{
+    "mySkillsLabel": "Offer at: Roma (Location ID: 36654)",
+    "tqOneLabel": "Roma"
+},
+{
+    "mySkillsLabel": "Offer at: Southbank (Location ID: 35212)",
+    "tqOneLabel": "South Bank"
+},
+{
+    "mySkillsLabel": "Offer at: Southport (Location ID: 36669)",
+    "tqOneLabel": "Southport"
+},
+{
+    "mySkillsLabel": "Offer at: Springfield (Location ID: 36655)",
+    "tqOneLabel": "Springfield"
+},
+{
+    "mySkillsLabel": "Offer at: The Whitsundays (Location ID: 36645)",
+    "tqOneLabel": "Whitsundays (Cannonvale)"
+},
+{
+    "mySkillsLabel": "Offer at: Thursday Island (Location ID: 36646)",
+    "tqOneLabel": "Thursday Island"
+},
+{
+    "mySkillsLabel": "Offer at: Toowoomba (Location ID: 36656)",
+    "tqOneLabel": "Toowoomba"
+},
+{
+    "mySkillsLabel": "Offer at: Townsville (Location ID: 36647)",
+    "tqOneLabel": "Townsville (Pimlico)"
+},
+{
+    "mySkillsLabel": "Offer at: Trade Training Centre (Location ID: 36631)",
+    "tqOneLabel": "Townsville (Trade Training Centre Bohle)"
+},
+{
+    "mySkillsLabel": "Offer at: Warwick (Location ID: 36657)",
+    "tqOneLabel": "Warwick"
+}];
 
 /*
 "Course Code"
